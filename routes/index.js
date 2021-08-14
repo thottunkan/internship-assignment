e81e43143937e1202a1b8697c5e5043344737dc8
AMBADI NARAYAN <ambadinarayan98@gmail.com>
2021-08-14 21:49:36 +0530

    index product added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var productHelper = require("../helpers/productHelpers")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { normal:true,title: 'Express' });
+  productHelper.getAllProducts().then((products)=>{
+    res.render('index', { normal:true,title: 'index',products:products });  
+  })
+  
 });
 
 module.exports = router;
